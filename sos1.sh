210b4a54202e2718b3cf2ce992c44df8bee4c7d2
liaoxi98 <92308987+liaoxi98@users.noreply.github.com>
2022-03-01 22:31:10 +0800

    Add files via upload

---
+++ sos1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash	
+InPath='./output/1'; # 输入路径	
+OuPath='./output/09_MOD09EVI_SOS';	# 输出路径
+	
+module unload gcc;	
+module load gcc/7.2.0;	
+	
+#rm -rf   $OuPath/	
+mkdir -p $OuPath/	
+# image name	
+	
+num=1;	
+min_data=1	
+max_data=21	
+	
+#Thread=1;	
+for InName in $(ls $InPath/*tif); do	
+  InFile=$(echo $InName | awk -F/ '{print $NF}');	
+  OuFile=$(echo $InFile | sed 's/Sin/WGS/g') ;	
+  OuName=$OuPath/$OuFile'_SOS.tif';	
+  	
+  if [ $num -ge $min_data ] && [ $num -lt $max_data ]		
+    	then echo $InFile		
+        		
+        echo $num	
+			
+		echo $InName #OuName #InFile #OuName  	
+		./open_MP_SOS $InName $OuName 	
+			
+  	
+  fi	
+  	
+num=$(( num+1 ));	
+	
+done	
+wait	
